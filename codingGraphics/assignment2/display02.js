672d342113defcf858bb4dca11186b18b3824d28
Akhil Batra <akhilbatra@berkeley.edu>
2015-03-13 00:14:14 -0700

    auto commit

--- codingGraphics/assignment2/display02.js
+++ codingGraphics/assignment2/display02.js
@@ -5,22 +5,22 @@ $(document).ready(function()
         $(".as2 .myComments").slideToggle(650);
     });
 
-    $(".as2 .step1").click(function()
+    $(".as2 .step1, .as2 .step2, .as2 .step25, as2 .screwups").click(function()
     {
-        $(".as2 .pic1").fadeSelfIn();
+        $(".as2 .titleChoice").fadeOut(1000, function()
+        {
+            $(".as2 .titleChoice").text("Click here to choose another milestone")
+                .fadeIn(1000)
+                .css("cursor", "pointer");
+        });
+        var pic = this;
+        $(".as2 .choices").fadeOut(1000, function()
+        {
+            $(pic).fadeIn(1000);
+        });
+
     });
-    $(".as2 .step2").click(function()
-    {
-        $(".as2 .pic2").fadeSelfIn();
-    });
-    $(".as2 .step25").click(function()
-    {
-        $(".as2 .pic25").fadeSelfIn();
-    });
-    $(".as2 .screwups").click(function()
-    {
-        $(".as2 .picMis").fadeSelfIn();
-    });
+
     $(".as2 .titleChoice").click(function(e)
     {
         if ($(".as2 .titleChoice").text() == "Click here to choose another milestone")
@@ -38,21 +38,3 @@ $(document).ready(function()
         }
     });
 });
-
-$.fn.fadeSelfIn = function()
-{
-    return this.each(function()
-    {
-        $(".as2 .titleChoice").fadeOut(1000, function()
-        {
-            $(".as2 .titleChoice").text("Click here to choose another milestone")
-                .fadeIn(1000)
-                .css("cursor", "pointer");
-        });
-        var pic = this;
-        $(".as2 .choices").fadeOut(1000, function()
-        {
-            $(pic).fadeIn(1000);
-        });
-    });
-}
