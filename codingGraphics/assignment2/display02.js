7bc108ea08c29dbdac4b6b215cd8d8cceacf2038
Akhil Batra <akhilbatra@berkeley.edu>
2015-03-22 14:24:13 -0700

    auto commit

--- codingGraphics/assignment2/display02.js
+++ codingGraphics/assignment2/display02.js
@@ -46,9 +46,9 @@ $(document).ready(function()
         {
             $(".as2 .titleChoice").fadeOut(1000, function()
             {
-                $(".as2 .titleChoice").text("Pick one of the following milestones I had in this project:")
-                    .fadeIn(1000)
-                    .css("cursor", "");
+                $(".as2 .titleChoice").text("Pick one of the following milestones")
+                    .css("cursor", "")
+                    .fadeIn(1000);
             });
             $(".as2 .pic").fadeOut(1000, function()
             {
