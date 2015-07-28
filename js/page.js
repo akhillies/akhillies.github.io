5d3a0a2c63ce34625f1c1307f887303c7a74e71c
Akhil Batra <akhilbatra@berkeley.edu>
2015-07-28 01:36:38 -0700

    auto commit

--- js/page.js
+++ js/page.js
@@ -37,7 +37,6 @@ app.controller('codingCtrl', function ($scope) {
 
 app.controller('imageryCtrl', function ($scope, $routeParams) {
         $scope.pageClass = "imageryPage";
-        console.log($routeParams);
         $('.imagery a[data-target="#' + $routeParams.tabid + '"]').click();
     });
 
