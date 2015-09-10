3761120a3a7af9129c513c493cf6245380962450
Akhil Batra <akhilbatra@berkeley.edu>
2015-09-10 10:03:03 -0700

    auto commit

--- js/page.js
+++ js/page.js
@@ -15,9 +15,9 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
             .when("/404", {templateUrl: "html/error.html", controller: "errorCtrl"})
             .otherwise({redirectTo: '/404'});
 
-        $locationProvider.html5Mode({
-            enabled: true
-        });
+        // $locationProvider.html5Mode({
+ //            enabled: true
+        // });
     }]);
 
 
