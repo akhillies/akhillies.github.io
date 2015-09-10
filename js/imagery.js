66771589a94ba71edcefff0075821cc6571c7ce0
Akhil Batra <akhilbatra@berkeley.edu>
2015-09-10 11:06:59 -0700

    auto commit

--- js/imagery.js
+++ js/imagery.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
         $('.imagery a[data-toggle="tab"]').on("hide.bs.tab", function(event) {
             if(history.pushState)
             {
-                history.pushState(null, null, '#/imagery/' + event.relatedTarget.dataset.target.substring(1));
+                history.replaceState(null, null, '#/imagery/' + event.relatedTarget.dataset.target.substring(1));
                 console.log('imagery/' + event.relatedTarget.dataset.target.substring(1));
             }
             else
