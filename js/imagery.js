544602029daf7d47e0b41cc2c0b3bc820c8543c4
Akhil Batra <akhilbatra@berkeley.edu>
2015-09-10 10:58:08 -0700

    auto commit

--- js/imagery.js
+++ js/imagery.js
@@ -2,11 +2,13 @@ $(document).ready( function() {
         $('.imagery a[data-toggle="tab"]').on("hide.bs.tab", function(event) {
             if(history.pushState)
             {
-                history.pushState(null, null, '/#/imagery/' + event.relatedTarget.dataset.target.substring(1));
+                history.pushState(null, null, '#/imagery/' + event.relatedTarget.dataset.target.substring(1));
+                console.log('#/imagery/' + event.relatedTarget.dataset.target.substring(1));
             }
             else
             {
                 location.hash = '#/imagery/' + event.target;
+                console.log('#/imagery/' + event.target);
             }
         });
     });
